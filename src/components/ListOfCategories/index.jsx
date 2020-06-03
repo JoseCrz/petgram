394104dbe99506a4423028ee13e3716377ccb287
Jose Cuevas <josecueram@gmail.com>
2020-06-03 00:38:06 -0500

    added data fetching to ListOfCategories component

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../mocks/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgram-backend-one.now.sh/categories')
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <List>
       {categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)}
