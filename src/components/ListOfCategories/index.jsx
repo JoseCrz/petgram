da366da645023ccdfc043a1022992e4784518a96
Jose Cuevas <josecueram@gmail.com>
2020-06-03 15:26:40 -0500

    added mini fixed ListOfCategories

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -5,7 +5,9 @@ import axios from 'axios'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
+  // ? Effect to fetch data
   useEffect(() => {
     const fetchCategories = async () => {
       try {
@@ -19,9 +21,30 @@ export const ListOfCategories = () => {
     fetchCategories()
   }, [])
 
-  return (
-    <List>
+  // ? Effect to determine whether or not to render the mini fixed list
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 210
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,6 +4,21 @@ export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+
+  &.fixed {
+    background: #FFF;
+    border-radius: 25px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, .25);
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    top: 0;
+    transform: scale(.8);
+    z-index: 9999;
+  }
 `
 export const Item = styled.li`
   padding: 0 8px;
