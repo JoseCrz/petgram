40709e3d8f116232a8cbef308cfc95bcf94f5912
Jose Cuevas <josecueram@gmail.com>
2020-06-02 23:24:00 -0500

    created Logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyle'
+import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
---
+++ src/components/Logo/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 250px;
+  margin: 0 auto;
+`
