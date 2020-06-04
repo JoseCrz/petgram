b6ab2c55b74c540989e9a9bd29bf203fa9b21c91
Jose Cuevas <josecueram@gmail.com>
2020-06-04 00:22:16 -0500

    modularized intersection observer PhotoCard -> useNearScreen custom hook

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,37 +1,15 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
 import { Article, ImageWrapper, Image, Button } from './style'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-const loadPollyfill = async () => {
-  try {
-    await import('intersection-observer')
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
-  const [show, setShow] = useState(false)
   const [like, setLike] = useLocalStorage(key, false)
-  const element = useRef(null)
-
-  useEffect(() => {
-    if (!window.IntersectionObserver) loadPollyfill()
-
-    const observer = new window.IntersectionObserver(entries => {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
-    })
-
-    observer.observe(element.current)
-  }, [element])
+  const [show, element] = useNearScreen()
 
   const Icon = like ? AiFillHeart : AiOutlineHeart
 
