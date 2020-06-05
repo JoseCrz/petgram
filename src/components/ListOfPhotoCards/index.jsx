3bca8fa97de72948bcda844d82f7a2ec893a870c
Jose Cuevas <josecueram@gmail.com>
2020-06-05 12:51:42 -0500

    installed & implemented react-apollo apollo-boost

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
 import { PhotoCard } from '../PhotoCard'
-import { photos } from '../../mocks/db.json'
-export const ListOfPhotoCards = () => {
+
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
       {photos.map(photo => <li key={photo.id}> <PhotoCard {...photo} /> </li>)}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
