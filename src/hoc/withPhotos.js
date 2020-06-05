794a020961368b5cf9a29d500382617e66f9e28a
Jose Cuevas <josecueram@gmail.com>
2020-06-05 18:01:33 -0500

    minor refactor in withPhotos

--- src/hoc/withPhotos.js
+++ src/hoc/withPhotos.js
@@ -1,15 +1,17 @@
 import { graphql } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
-export const withPhotos = graphql(gql`
-  query getPhotos($categoryId: ID) {
-    photos(categoryId: $categoryId) {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
+const GET_PHOTOS = gql`
+query getPhotos($categoryId: ID) {
+  photos(categoryId: $categoryId) {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
   }
-`)
+}
+`
+
+export const withPhotos = graphql(GET_PHOTOS)
