ba5154b5ca593b3e638d91650e46689555c31f7a
Jose Cuevas <josecueram@gmail.com>
2020-06-05 13:40:30 -0500

    modularized ListOfPhotoCards component logic into -> /hoc & /containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './containers/ListofPhotoCards'
 import { Logo } from './components/Logo'
 
 export const App = () => (
--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
 
 import { PhotoCard } from '../PhotoCard'
 
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
       {photos.map(photo => <li key={photo.id}> <PhotoCard {...photo} /> </li>)}
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
