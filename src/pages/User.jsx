e488a19d8e10888b20534f4b1c225d717afed900
Jose Cuevas <josecueram@gmail.com>
2020-06-07 14:11:39 -0500

    implemented Layout in User Page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import { Context } from '../Context'
+import { Layout } from '../components/Layout'
 import { ActionButton } from '../components/ActionButton'
 
 export const User = () => {
   const { removeAuth } = useContext(Context)
   return (
-    <>
+    <Layout title='User' description='User settings'>
       <h1>User</h1>
       <ActionButton onClick={removeAuth} text='Sign out' />
-    </>
+    </Layout>
   )
 }
