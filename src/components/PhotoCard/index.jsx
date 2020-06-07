fb5b99f8f9bdb116cfe7cbfda2cc0088894714f8
Jose Cuevas <josecueram@gmail.com>
2020-06-07 15:57:45 -0500

    implemented prop-types in PhotoCard component

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from '@reach/router'
+import PropTypes from 'prop-types'
 import { Article, ImageWrapper, Image } from './style'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
@@ -37,4 +38,21 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       }
     </Article>
   )
+}
+
+PhotoCard.PropTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: (props, propName, componentName) => {
+    const propValue = props[propName]
+
+    if (propValue === undefined) {
+      return new Error(`${propName} value must be defined`)
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} value must be greater than 0`)
+    }
+  }
 }
