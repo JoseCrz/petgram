bec9ed01f01f8048d07b4f7e5830815d7757ec92
Jose Cuevas <josecueram@gmail.com>
2020-06-07 15:53:26 -0500

    implemented prop-types in ActionButton component

--- src/components/ActionButton/index.jsx
+++ src/components/ActionButton/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Button } from './style'
 
 export const ActionButton = ({ onClick, text, disabled, type }) => {
   return (
     <Button disabled={disabled} onClick={onClick} type={type}>{text}</Button>
   )
+}
+
+ActionButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  type: PropTypes.string
 }
