6de1ffb0456e24d60c8083b5133cdb8f1f321639
Jose Cuevas <josecueram@gmail.com>
2020-06-07 14:08:00 -0500

    implemented Layout in NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
 
 export const NotFound = () => {
   return (
-    <h1>We couldn't find that :(</h1>
+    <Layout title='404' description='Error 404: not found'>
+      <h1>We couldn't find that :(</h1>
+    </Layout>
   )
 }
