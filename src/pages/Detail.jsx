37c32239de070ed251add01a275d6de3c86ac95a
Jose Cuevas <josecueram@gmail.com>
2020-06-07 14:06:04 -0500

    implemented Layout in Detail Page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
 import { PhotoCardWithQuery } from '../containers/PhotoCardWithQuery'
 
 export const Detail = ({ detailId }) => {
   return (
-    <PhotoCardWithQuery id={detailId} />
+    <Layout title='Photo' description='Take a closer look'>
+      <PhotoCardWithQuery id={detailId} />
+    </Layout>
   )
 }
