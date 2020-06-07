9d7ad13f63e7dd31eed741e81d982b4f793c2f65
Jose Cuevas <josecueram@gmail.com>
2020-06-07 14:03:57 -0500

    implemented Layout in Favs page

--- src/pages/Favs.jsx
+++ src/pages/Favs.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
 import { GetFavs } from '../containers/GetFavs'
 
 export const Favs = () => {
   return (
-    <>
+    <Layout title='Favs' description='Find your favorite pets'>
       <h1>Favs</h1>
       <GetFavs />
-    </>
+    </Layout>
   )
 }
